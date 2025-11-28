fa93d75d247a2a3ff23da52321dbf76289917e7f
exon <throwawayact69xd@gmail.com>
2025-11-27 22:32:30 -0500

    better nav done client side

--- app/public/js/header.js
+++ app/public/js/header.js
@@ -1,8 +1,30 @@
-let curr = window.location.pathname == '/' ? '/home' : window.location.pathname;
+const pageMap = [
+  { name: 'home', path: '/' },
+  { name: 'blog', path: '/blog' },
+  { name: 'soon', path: 'https://sketchyfile.download?refer=exonauto.me' },
+  { name: 'admin', path: '/admin', authed: true }
+];
 
-fetch(`/api/headers?currPage=${curr}`)
-  .then(response => response.text())
-  .then(html => {
-    document.querySelector("nav").innerHTML += html;
-  })
-  .catch(console.error);
+async function init() {
+  const me = await (await fetch('/api/me')).json();
+  const authed = me.authed;
+
+  const nav = document.querySelector("nav");
+  const currName = window.location.pathname.substring(1) || "home";
+
+  for (const page of pageMap) {
+    if (page.authed && !authed) continue;
+
+    if (currName.startsWith(page.name)) continue;
+    
+    const a = document.createElement("a");
+    a.textContent = page.name;
+    a.href = page.path;
+    a.style.textDecoration = "underline";
+    a.style.marginRight = "10px";
+
+    nav.appendChild(a);
+  }
+}
+
+init();
