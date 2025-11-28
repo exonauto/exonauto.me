a54b4b9f324ec7e1d54da92c6ab70bf3f6b6dc9c
exon <throwawayact69xd@gmail.com>
2025-11-27 22:15:54 -0500

    nav api

--- src/api/api.js
+++ src/api/api.js
@@ -3,6 +3,29 @@ import { authentication, blogs, saveBlogs, generateImage } from '../utils/utils.
 
 const api = express.Router()
 
+const pages = [
+  { name: 'home', path: '/' },
+  { name: 'blog', path: '/blog' },
+  { name: 'soon', path: 'https://sketchyfile.download?refer=exonauto.me'}
+];
+
+api.get('/headers', (req, res) => {
+  let curr = req.query.currPage || '';
+  curr = curr.substring(1,curr.length)
+  let html = '';
+
+  for (const page of pages) {
+    if (curr.startsWith(page.name)) continue;
+    html += `<a style="text-decoration:underline" href="${page.path}">${page.name}</a>`;
+  }
+
+  if (req.session.authed) {
+    html += `<a style="text-decoration:underline" href="/admin">admin</a>`;
+  }
+
+  res.send(html);
+});
+
 api.use(authentication)
 
 api.get('/blogs', (req, res) => {
